738d537534565a672a15bc808c8e0c2d5021d559
Leonardo Lauermann <leolds010101@gmail.com>
2026-01-19 17:55:45 -0300

    removido toastr indevido

--- src/app/(medias)/not-found.tsx
+++ src/app/(medias)/not-found.tsx
@@ -1,16 +1,7 @@
-'use client'
-
-import { useEffect } from 'react';
-import { toast } from 'sonner';
-
 export default function NotFound() {
-    useEffect(() => {
-        toast.error('', {
-            description: 'Nenhum resultado encontrado para sua busca.',
-        });
-    }, []);
-
     return (
-        <p className='text-gray-400 font-medium'>Nenhum resultado encontrado para sua busca.</p>
-    )
+        <p className="text-gray-400 font-medium">
+            Nenhum resultado encontrado para sua busca.
+        </p>
+    );
 }
